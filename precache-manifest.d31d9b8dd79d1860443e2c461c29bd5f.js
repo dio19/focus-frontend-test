2304bb070871845e902bd5629d350788179d5121
dio19 <hernandezdionisiooscar@gmail.com>
2020-11-27 01:47:32 -0300

    Updates

---
+++ precache-manifest.d31d9b8dd79d1860443e2c461c29bd5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5e1fa14396b7dacd05ec714f97a275",
+    "url": "/dio19.github.io/focus-frontend-test/index.html"
+  },
+  {
+    "revision": "e223f991a2d65c65d727",
+    "url": "/dio19.github.io/focus-frontend-test/static/css/main.8712a04b.chunk.css"
+  },
+  {
+    "revision": "2f9aa2678465a309f7c2",
+    "url": "/dio19.github.io/focus-frontend-test/static/js/2.bac06f8c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dio19.github.io/focus-frontend-test/static/js/2.bac06f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e223f991a2d65c65d727",
+    "url": "/dio19.github.io/focus-frontend-test/static/js/main.6d23354f.chunk.js"
+  },
+  {
+    "revision": "cb47d55a1c055ace2f46",
+    "url": "/dio19.github.io/focus-frontend-test/static/js/runtime-main.cb22c4f7.js"
+  },
+  {
+    "revision": "efd54e736e9b6208710472c9c1479659",
+    "url": "/dio19.github.io/focus-frontend-test/static/media/Mont-Bold.efd54e73.ttf"
+  },
+  {
+    "revision": "5cf16df9fb7a9a4aafd56cb48be75cf2",
+    "url": "/dio19.github.io/focus-frontend-test/static/media/Mont-SemiBold.5cf16df9.ttf"
+  }
+]);
